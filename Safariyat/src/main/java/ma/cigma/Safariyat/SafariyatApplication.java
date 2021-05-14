b08700c53ea8b257cd1ab4d06c674a6485d40484
redouanejl <jredouane7@gmail.com>
2021-05-14 13:18:34 +0000

    Starting Project ...

---
+++ Safariyat/src/main/java/ma/cigma/Safariyat/SafariyatApplication.java
@@ -0,0 +1,13 @@
+package ma.cigma.Safariyat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafariyatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafariyatApplication.class, args);
+	}
+
+}
---
+++ Safariyat/src/test/java/ma/cigma/Safariyat/SafariyatApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.cigma.Safariyat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafariyatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
